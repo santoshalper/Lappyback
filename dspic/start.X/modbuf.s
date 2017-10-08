dd4e019745c030292f01ee3d7b65991041ee7c20
Vijay Natarajan <vijaynat91@gmail.com>
2017-10-08 17:47:04 -0400

    modulo addressing untested

---
+++ dspic/start.X/modbuf.s
@@ -0,0 +1,23 @@
+.global _InpB, _W2OB , _R2OB
+_InpB:
+ mov.w [w1], w10
+ mov.w w0, [w10++]
+ mov.w w10, [w1]
+ return
+
+_W2OB:
+ mov.w [w1], w11
+ mov.w w0, [w11++]
+ mov.w w11, [w1]
+ return
+ 
+_R2OB:
+ mov.w w0, w1
+ mov.w [w1], w11
+ mov.w [w11++], w0
+ mov.w w11, [w1]
+ return
+.end
+ 
+  
+
